c7ca6fcc01be2839e7b544f87b0bb296302dc2a0
emreeken183425 <emreeken1834@gmail.com>
2022-08-24 23:04:53 +0300

    form

--- tekrar/DJANGOFORMS/student/views.py
+++ tekrar/DJANGOFORMS/student/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from student.models import Student
+from .forms import StudentForm
+
+
+def index(request):
+    return render(request, 'student/index.html')
+
+
+# def student_page(request):
+#     return render(request, 'student/student.html')
+
+
+# def student_page(request):
+    # print(request.POST)
+    # print(request.FILES)
+    # form = StudentForm()
+    # context = {
+    #     'form': form
+    # }
+
+    # return render(request, 'student/student.html', context)
+
+
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             student_data = {
+#                 "first_name": form.cleaned_data.get('first_name'),
+#                 "last_name": form.cleaned_data.get("last_name"),
+#                 "number": form.cleaned_data.get("number"),
+#                 "profile_pic": form.cleaned_data.get("profile_image")
+#             }
+#             # Student.objects.create(**student_data)
+#             student = Student(**student_data)
+#             student.save()
+#             return redirect('student')  #redirect tekrar get pozisyonu yani kullanıcı giriş kısmına gelmesine yarar
+#     context = {
+#         'form': form
+#     }
+    
+#     return render(request, 'student/student.html', context)
+
+def student_page(request):
+    form=StudentForm()
+    if request.method =="POST":
+        form==StudentForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("student")
+
+    context={
+        'form':form
+    }        
+    return render(request,'student/student.html',context)
