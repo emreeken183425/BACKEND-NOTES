637912ad2a6537676cb528709b42c1f2e7b8afcf
emreeken183425 <emreeken1834@gmail.com>
2022-10-06 21:18:13 +0300

    change

--- django/projects/QuizApp/main/urls.py
+++ django/projects/QuizApp/main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('student_api.urls')),
-    path('user/', include('user_api.urls')),
+    path('nested_admin/', include('nested_admin.urls')),
+    path('quiz/', include('quiz.urls')),
 ]
